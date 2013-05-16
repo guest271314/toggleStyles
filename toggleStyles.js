e2be89257e2affaca3dc1a8f9bce7dbe459a886b
guest271314 <guest271314@gmail.com>
2013-05-15 22:40:40 -0600

    Update toggleStyles.js

--- toggleStyles.js
+++ toggleStyles.js
@@ -1,18 +1,11 @@
-/* toggleStyles.js @guest271314
+/* toggleStyles.js guest271314 http://guest271314.com
 
 Toggle CSS on or off in browser. 
 
 Reference: http://superuser.com/questions/447269/is-there-any-way-to-view-a-webpage-without-styles-in-chrome 
 
 http://www.opensource.org/licenses/MIT
- 
-Copyright (C) 2013 guest271314 http://guest271314.com
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 /* Load jquery. */
@@ -57,5 +50,5 @@ return d};
 }); 
 
 }
-},15);
+},1000);
 })();
