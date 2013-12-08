a8cd2fd51bb725e2623f37d994bb9fa28e4fd12f
guest271314 <guest271314@gmail.com>
2013-12-08 08:01:56 -0800

    Update toggleStyles.js
    Changes : 
    1) jQuery library to `jquery-1.10.1.min.js`; 
    2) `s.parentNode.insertBefore(s,jq)` to `s.appendChild(jq)`.
    
    To fix `NetworkError` and `ReferenceError` when loading jQuery from CDN.

--- toggleStyles.js
+++ toggleStyles.js
@@ -1,15 +1,17 @@
-/* toggleStyles.js (c)2013 guest271314 http://guest271314.com http://www.opensource.org/licenses/MIT
+/*! toggleStyles.js (c)2013 guest271314 http://guest271314.com http://www.opensource.org/licenses/MIT
 * Toggle styles on or off in browser.
-* Reference: http://superuser.com/questions/447269/is-there-any-way-to-view-a-webpage-without-styles-in-chrome */
+* Reference: http://superuser.com/questions/447269/is-there-any-way-to-view-a-webpage-without-styles-in-chrome 
+* Updated: 2013-12-08
+*/
 /* Request jQuery, log success. */
 (function() {
     var jq = document.createElement('script');
     jq.type = 'text/javascript';
     jq.id = 'jq';
-    jq.src = ('http:' == document.location.protocol ? 'http://code.jquery.com/jquery-2.0.0.min.js': 'jquery-2.0.0.min.js');
+    jq.src = ('http:' == document.location.protocol ? 'http://code.jquery.com/jquery-1.10.1.min.js': 'jquery-1.10.1.min.js');
     jq.dataset[status] = 'jquery ready';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(s, jq);
+    var s = document.getElementsByTagName('head')[0];
+    s.appendChild(jq);
     console.log(jq.dataset[status]);
     document.getElementsByTagName('html')[0].dataset[status] = 'styles on';
 })();
