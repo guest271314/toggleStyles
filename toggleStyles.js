b872c22c9148a34ed7d8f717a32753dda7bbac72
guest271314 <guest271314@gmail.com>
2014-01-05 22:30:20 -0800

    Update toggleStyles.js

--- toggleStyles.js
+++ toggleStyles.js
@@ -1,34 +1,47 @@
-/*! toggleStyles.js (c) 2013, 2014 guest271314 http://guest271314.com 
+ /*! toggleStyles.js (c) 2013, 2014 guest271314 http://guest271314.com 
 * Toggle styles on or off in browser.
-* Reference: http://superuser.com/questions/447269/is-there-any-way-to-view-a-webpage-without-styles-in-chrome 
 * License: MIT http://www.opensource.org/licenses/MIT
-* Updated: 2014-01-02
+* Reference: http://superuser.com/questions/447269/is-there-any-way-to-view-a-webpage-without-styles-in-chrome 
+* Updated: 2014-01-05
 */
 
-/* Request jQuery, log success (optional) */
-(function j() {
-    callback = setTimeout(function() {
-        toggleStyles()
-    }, 5000);    
-    var q = false;
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = 'true';
-    script.id = 'jq';
-    script.src = 'https://code.jquery.com/jquery-1.10.2.min.js';
-    script.onload = script.onreadystatechange = function() {
-        q = ((!s && (!this.readyState || this.readyState == 'complete')) ? true : false);        
-        document.getElementsByTagName('html')[0].dataset.progress = 'jquery ' + window.jQuery().jquery;+ ' ready';        
-        return ((q && !!jQuery) === true ? callback : j())    
-    };
-    var head = document.getElementsByTagName('head')[0];
-    head.insertBefore(script, head.firstChild);    
-    return String('loading ' + script.src.substr(24, 20) + ' at ' + (new Date()).toJSON())
+    /* If jQuery not defined, request from code.jquery.com, 
+       log success, call toggleStyles() */
+(function requestCheck(url, id, callback, check) {
+    check = window.jQuery;
+    return !((check === undefined) ? (function _callback() {
+        callback = setTimeout(function() {
+            return ((document.getElementById('jq') && window.jQuery) ? toggleStyles() : callback)
+        }, 1000);
+        url = 'https://code.jquery.com/jquery-1.10.2.min.js';
+        id = 'jq';
+        var status = false;
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'application/javascript';
+        script.async = 'async';
+        script.jsonp = 'callback';
+        script.id = id;
+        script.src = url + '?callback='
+        script.onload = script.onreadystatechange = function() {
+            status = ((!status && (!this.readyState || this.readyState == 'complete')) ? true : false);
+            return callback
+        };
+        head.insertBefore(script, head.firstChild);
+        return console.log(String('loading ' + script.src + ' at ' + (new Date()).toJSON()))
+    }()) : 
+    /* When jQuery is defined, call toggleStyles() */
+    toggleStyles());
 })();
-/* toggleStyles.js */
-function toggleStyles() {
-    'use strict';
-    /* Save author styles, clear author styles, log success. Usage: clearStyle() */
+
+    /*! toggleStyles.js (c) 2014 guest271314 MIT License 
+        Usage: toggleStyles() */
+function toggleStyles($) {
+    $ = window.jQuery;
+    $('html')[0].dataset.progress = 'jquery ' + window.jQuery().jquery + ' ready at ' + (new Date()).toJSON();
+
+    /* Save author styles, clear author styles, log success.     
+       Usage: clearStyle(). (https://github.com/guest271314/toggleStyles/blob/master/clearStyle.min.js) */
     function clearStyle() {
         var $clone = $('html').contents().clone(true);
         $('html').data('styleReset', $clone);
@@ -41,9 +54,9 @@ function toggleStyles() {
         $('html')[0].dataset.status = off;
     /* Log style status at console (optioanl) */
         window.console.log($('html').data('styleStatus'));
-    }
-    ;
-    /* Reload saved author styles, log success. Usage: resetStyle() */
+    };
+    /* Reload saved author styles, log success. 
+       Usage: resetStyle() */
     function resetStyle() {
         var on = 'styles on';
         var $_clone = $('html').data('styleReset');
@@ -52,25 +65,20 @@ function toggleStyles() {
     /* Log style status at console (optioanl) */
         window.console.log($('html').data('styleStatus'));
     };
-    /* When jQuery is ready, toggle styles. Keyboard usage: Keydown space bar toggles styles on or off (optional) */
-    return (function toggleStyle(callback) {
+    /* When jQuery is ready, toggle styles. 
+        Keyboard usage: Keydown space bar 
+        toggles styles on or off (optional) */
+    return (function toggleStyle() {
         $('html').data('styleStatus', 'styles on');
         $('html')[0].dataset.status = 'styles on';
-        $('html')[0].dataset.progress = 'jquery ' + window.jQuery().jquery;+ ' ready';
     /* Log style status at console (optioanl) */
         window.console.log($('html').data('styleStatus') + ' ' + $('html')[0].dataset.progress);
-        var keys = ('keypress' || 'keydown');
-        $('body').on(keys, 
+        $('body').on('keydown', 
         function(toggle, _style) {
-            toggle = {
-                keyCode: 32
-            };
-            if (toggle) {
+            if (toggle.which === 32) {
                 _style = ('styles on' === $('html').data('styleStatus') ? clearStyle() : resetStyle());
                 return _style
             };
         });
-        if (callback != undefined) { clearTimeout(callback) };
-        return $('html')[0].dataset.progress
-    }());
+    })()
 };
